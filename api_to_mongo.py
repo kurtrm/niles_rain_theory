458d1d027c5fe2b3cafe2d36e60935d041a664a9
Kurt Maurer <kurtrm@gmail.com>
2018-04-06 17:24:16 -0700

    got the formatting string going.

--- api_to_mongo.py
+++ api_to_mongo.py
@@ -2,9 +2,11 @@
 Load data into mongo database.
 """
 import requests
+import yaml
+
+from typing import List, Dict
 
 from pymongo import MongoClient
-from typing import List, Dict
 
 
 def load_mongo_db(db: str, collections: str, items: List[Dict]) -> None:
@@ -16,3 +18,19 @@ def load_mongo_db(db: str, collections: str, items: List[Dict]) -> None:
     collection = database[collections]
     collection.insert_many(items)
     mc.close()
+
+
+def main():
+    """
+    Load stuff from api.
+    """
+    url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data'
+    '?datasetid=GHCND'
+    '&locationid=CITY:US530018'
+    '&startdate={}'
+    '&enddate={}'
+
+    with open('.secrets/noaa_api_key.yaml') as f:
+        token = yaml.load(f)
+
+    response = requests.get(url=url, params=None, headers=token)
